6dfabdc4866dac971b6239145c9c25903f3e7a1f
Adrian <adrian@travelio.com>
2017-07-12 17:52:45 +0700

    Add issue navigate using path and route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Router } from '../routes';
 
 class Index extends Component {
   render() {
     return (
       <div>
         Index Pages
+        <div>
+          <button onClick={() => { Router.pushRoute('search', { id: 1, name: 'abc' }); }}>
+            Search using route name
+          </button>
+          <button onClick={() => { Router.pushRoute('/search', { id: 1, name: 'abc' }); }}>
+            Search using path
+          </button>
+        </div>
       </div>
     );
   }
--- routes.js
+++ routes.js
@@ -4,4 +4,4 @@ const routes = module.exports = nextRoutes();
 const localeRegExp = ':locale(id|en)?';
 
 routes.add('index', `/${localeRegExp}`);
-routes.add('search', `/${localeRegExp}/search/:urlName`);
+routes.add('search', `/${localeRegExp}/search`);
